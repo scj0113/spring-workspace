ffa0e5431035dbe98d9eb43c2235c4e5b95dad6b
scj0113 <scj0113@naver.com>
2023-09-06 12:18:28 +0900

    20230906 spring

--- 07_RestfulAPI/src/main/java/com/kh/api/service/PhoneServiceImpl.java
+++ 07_RestfulAPI/src/main/java/com/kh/api/service/PhoneServiceImpl.java
@@ -1,47 +1,50 @@
-package com.kh.api.impl;
+package com.kh.api.service;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.kh.api.dao.PhoneDAO;
 import com.kh.api.model.Phone;
 import com.kh.api.model.UserInfo;
 
-public class PhoneDAOImpl implements PhoneDAO{
+@Service
+public class PhoneServiceImpl implements PhoneService{
 
+	
+	@Autowired
+	private PhoneDAO dao;
+	
+	
 	@Override
 	public int insert(Phone phone) {
-		// TODO Auto-generated method stub
-		return 0;
+		return dao.insert(phone);
 	}
 
 	@Override
 	public int update(Phone phone) {
-		// TODO Auto-generated method stub
-		return 0;
+		return dao.update(phone);
 	}
 
 	@Override
 	public int delete(String num) {
-		// TODO Auto-generated method stub
-		return 0;
+		return dao.delete(num);
 	}
 
 	@Override
 	public Phone select(String num) {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.select(num);
 	}
 
 	@Override
 	public List<Phone> select() {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.select();
 	}
 
 	@Override
 	public UserInfo select(UserInfo user) {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.select(user);
 	}
 
 
